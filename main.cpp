387b8aaf80fc57d48e1b17a3a068b881b8a7772d
Adam Sawicki <adam2sawicki@gmail.com>
2020-01-30 21:54:56 +0100

    Add user input handling, refactor code

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 //  main.cpp
 //  labirynth
 //
-//  Created by Adam on 11/12/2019.
-//  Copyright © 2019 Adam. All rights reserved.
+//
+//
 //
 
 #include <iostream>
@@ -29,9 +29,11 @@ void printMaze(vector<vector<bool> > grid) {
         }
         cout << " " << row << endl;
     }
+    cout << endl;
+
 }
 
-vector<vector<int> > checkForNeighbours(int x, int y, vector<vector<bool> > grid) {
+vector<vector<int> > checkForNeighbours(int x, int y, vector<vector<bool> > grid, bool printStep) {
     vector<vector<int>>  neighbours;
     //top
     if (y - 2 > 0 && grid[y - 2][x] == true) {
@@ -49,10 +51,13 @@ vector<vector<int> > checkForNeighbours(int x, int y, vector<vector<bool> > grid
     if (x + 2 < grid[y].size() && grid[y][x + 2] == true) {
         neighbours.push_back({y, x +2, y, x + 1});
     }
-    cout << "Neighbours found: " << endl;
-    for (int i = 0; i < neighbours.size(); i++) {
-        cout << i << ". y: " << neighbours[i][0] << " x: " << neighbours[i][1] << endl;
+    if (!neighbours.empty() && printStep) {
+        cout << "Neighbours found: " << endl;
+        for (int i = 0; i < neighbours.size(); i++) {
+            cout << i << ". y: " << neighbours[i][0] << " x: " << neighbours[i][1] << endl;
+        }
     }
+    
     return neighbours;
 }
 void makePath(vector<int> coordinates, vector<vector<bool> >& grid) {
@@ -64,43 +69,131 @@ void makePath(vector<int> coordinates, vector<vector<bool> >& grid) {
     grid[y2][x2] = false;
 }
 
+vector<vector<bool> > generateMaze(int width, int height, bool printStep) {
+        srand (time(NULL));
+        int x = 1;
+        int y = 1;
+        int rows = height;
+        int cols = width;
+        stack<vector<int>> visitedCells;
+        vector<vector<bool> > grid(rows, vector<bool>(cols));
+        // Fill grid with walls
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                grid[row][col] = true;
+            }
+        }
+        grid[y][x] = false;
+        visitedCells.push({y, x});
+        if (printStep) printMaze(grid);
+        
+        while (!visitedCells.empty()) {
+            vector<vector<int>> neighbours;
+            if (printStep)  {
+                neighbours = checkForNeighbours(x, y, grid, true);
+            } else {
+                neighbours = checkForNeighbours(x, y, grid, false);
+            }
+            if (!neighbours.empty()) {
+                int random = randomNumber(0, neighbours.size() - 1);
+                if (printStep) cout << "Random neighbour: " << random << endl;
+                vector<int> randomNeighbour = neighbours[random];
+                makePath(randomNeighbour, grid);
+                if (printStep) printMaze(grid);
+                x = randomNeighbour[1];
+                y = randomNeighbour[0];
+                visitedCells.push({y, x});
+            } else {
+                vector<int> cell = visitedCells.top();
+                visitedCells.pop();
+                x = cell[1];
+                y = cell[0];
+            }
+        }
+        
+        grid[0][1] = false;
+        grid[rows - 1][cols - 2] = false;
+        return grid;
+}
+
+void greet() {
+    cout << "Witaj w programie generującym labirynty ASCII" << endl
+    << "Autorzy: Adam Sawicki, Vitaliy Sklyar" << endl;
+}
+void showOptions() {
+    cout << "Wybierz opcję: " << endl
+    << "1. Wygeneruj Labirynt oraz pokaż jak powstawał." << endl
+    << "2. Wyjdź z programu." << endl;
+}
+
+int getWidth(){
+    cout << "Podaj szerokość labiryntu (liczba nieparzysta od 21 do 201)" << endl;
+    int width;
+    while (!(cin >> width) || (width % 2 == 0) || ((width < 21) || (width > 201))) {
+        cout << "Podaj szerokość labiryntu (liczba nieparzysta od 21 do 201)" << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    return width;
+}
+int getHeight(){
+    cout << "Podaj dlugość labiryntu (liczba nieparzysta od 11 do 101)" << endl;
+    int height;
+    while (!(cin >> height) || (height % 2 == 0) || ((height < 11) || (height > 201))) {
+        cout << "Podaj dlugosc labiryntu (liczba nieparzysta od 21 do 101)" << endl;
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    return height;
+}
+
+
 int main(int argc, const char * argv[]) {
-    srand (time(NULL));
-    int x = 1;
-    int y = 1;
-    int rows = 21;
-    int cols = 81;
-    stack<vector<int>> visitedCells;
-    vector<vector<bool> > grid(rows, vector<bool>(cols));
-    // Fill grid with walls
-    for (int row = 0; row < rows; row++) {
-        for (int col = 0; col < cols; col++) {
-            grid[row][col] = true;
+    int rows;
+    int cols;
+    bool loop = true;
+    int option;
+    greet();
+    while (loop) {
+        showOptions();
+        while (!(cin >> option) || ((option != 1) && (option != 2))) {
+            showOptions();
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        
+        switch (option) {
+            case 1: {
+                cols = getWidth();
+                rows = getHeight();
+                bool printStep;
+                int stepOption;
+                vector<vector<bool> > grid(rows, vector<bool>(cols));
+                cout << "Czy wyświetlać kroki?" << endl
+                << "1. Tak." << endl
+                << "2. Nie." << endl;
+                while (!(cin >> stepOption) || ((option != 1) && (option != 2))) {
+                    cout << "Czy wyświetlać kroki?" << endl
+                    << "1. Tak." << endl
+                    << "2. Nie." << endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }
+                if (stepOption == 1) {
+                    grid = generateMaze(cols, rows, true);
+                } else {
+                    grid = generateMaze(cols, rows, false);
+                }
+                printMaze(grid);
+                break;
+            }
+            case 2: {
+                loop = false;
+                break;
+            }
         }
     }
-    grid[y][x] = false;
-    visitedCells.push({y, x});
-    printMaze(grid);
- 
-    
-    while (!visitedCells.empty()) {
-        vector<vector<int>> neighbours = checkForNeighbours(x, y, grid);
-        if (!neighbours.empty()) {
-            int random = randomNumber(0, neighbours.size() - 1);
-            cout << "Random number: " << random << endl;
-            vector<int> randomNeighbour = neighbours[random];
-            makePath(randomNeighbour, grid);
-            printMaze(grid);
-            x = randomNeighbour[1];
-            y = randomNeighbour[0];
-            visitedCells.push({y, x});
-        } else {
-            vector<int> cell = visitedCells.top();
-            visitedCells.pop();
-            x = cell[1];
-            y = cell[0];
-        }
-    }
+    return 0;
 }
 
 
